62098ad1a53c0f26fd82c11a27b1867ea8645370
kaznak <kaznak.at.work@ipebble.org>
2022-12-22 01:41:54 +0900

    update

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import CreateTask from "./CreateTask";
+import Tasks from "./Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = (text, id) => ({
+    id,
+    text,
+    completed: false,
+  });
+
+  const addTask = (task) => {
+    const temp = [...tasks];
+    temp.push(createTask(task, tasks.length));
+    setTasks(temp);
+  };
+
+  const toggleCompleted = (id) => {
+    let temp = [...tasks];
+    const i = temp.findIndex((t) => t.id === id);
+    temp[i].completed = !temp[i].completed;
+    setTasks(temp);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CreateTask addTask={addTask} />
+      <Tasks tasks={tasks} toggleCompleted={toggleCompleted} />
     </div>
   );
 }
